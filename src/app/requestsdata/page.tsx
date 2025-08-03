b7b3a9cf85e2c68fcaa7ac5159bfa27672b721f4
Humberto Ribeiro <humbertoemaus@gmail.com>
2025-08-03 12:32:01 -0300

    feat: update footer and requestdata page

--- src/app/requestsdata/page.tsx
+++ src/app/requestsdata/page.tsx
@@ -2,7 +2,7 @@ import BemefitComponent from "@/components/bemefit";
 
 export default function RequestsData() {
     return (
-        <section className="w-full flex justify-center p-6">
+        <section className="w-full lg:min-h-[calc(100vh-328px)] min-h-[calc(100vh-288px)] flex justify-center p-6">
             <BemefitComponent />
         </section>
     );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,14 +9,14 @@ export default function FooterComponent() {
             <section className="w-full max-w-5xl flex justify-between">
                 <ul className="w-full max-w-56 flex flex-col gap-2">
                     <li>Empresa especializada em soluções financeiras como:</li>
-                    <li>EMPRÉSTIMOS</li>
-                    <li>NEGOCIAÇÕES</li>
-                    <li>SAQUE FGTS</li>
-                    <li>ENTRE OUTROS…</li>
+                    <li className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>EMPRÉSTIMOS</li>
+                    <li className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>NEGOCIAÇÕES</li>
+                    <li className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>SAQUE FGTS</li>
+                    <li className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>ENTRE OUTROS…</li>
                 </ul>
-                <div className="w-full max-w-56">
-                    <p>Correspondente bancário autorizado pela <strong className="text-blue-700">FEBRABAN</strong> Código: <strong className="text-blue-700">546.55677.777-90</strong></p>
-                    <p>CNPJ: <strong className="text-blue-700">41.313.421/0001-50</strong></p>
+                <div className="w-full max-w-56 text-end">
+                    <p>Correspondente bancário autorizado pela <strong className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>FEBRABAN</strong> Código: <strong className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>546.55677.777-90</strong></p>
+                    <p>CNPJ: <strong className={`${pathname !== '/' && 'text-yellow-500'} text-blue-700`}>41.313.421/0001-50</strong></p>
                 </div>
             </section>
         </footer>
