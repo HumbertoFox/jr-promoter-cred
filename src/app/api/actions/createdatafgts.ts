6fff1b59f962cb524d7cec63dee699e867556e57
Humberto Ribeiro <humbertoemaus@gmail.com>
2025-08-05 09:03:33 -0300

    feat: update env phone number

--- src/app/api/actions/createdatafgts.ts
+++ src/app/api/actions/createdatafgts.ts
@@ -14,5 +14,5 @@ export async function createDataFgts(state: FormStateFgts, formData: FormData): 
 
     const { name, cpf, phone } = validatedFields.data;
 
-    redirect(`https://wa.me/5581997933386?text=nome: ${name}%0Acpf: ${cpf}%0Atelefone: ${phone}`);
+    redirect(`https://wa.me/${process.env.MY_NUMBER_PHONE}?text=nome: ${name}%0Acpf: ${cpf}%0Atelefone: ${phone}`);
 }
